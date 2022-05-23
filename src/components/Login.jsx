a8aea70505ad1e0094d94adcc508b8e17c01f83b
Arthur <arthur_2335@hotmail.com>
2022-05-23 15:01:56 -0400

    added login with firebase.auth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import { Button, Form, Input } from "antd";
-import FormItem from "antd/lib/form/FormItem";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCYlLLUHWzzgqDjvRfJnDw2dOeHzjfUGP8",
@@ -11,19 +11,31 @@ const firebaseConfig = {
   appId: "1:646153161260:web:b41567efed7bf588895f89"
 };
 
-const app = initializeApp(firebaseConfig);
 
 export default function Login() {
+    const handelLogin = ({email, password}) => {
+        const app = initializeApp(firebaseConfig); //connect to firebase
+        const auth = getAuth(app) // connect to firebase/auth
+            // login with Firebase Auth
+            signInWithEmailAndPassword(auth, email, password)
+            .then(res => console.log(res.user))
+            .catch(console.error)
+        }
     return (
         <section style={{padding: '2em'}}>
         <Form 
+        onFinish={handelLogin}
         labelCol={{span: 8}}
         wrapperCol={{span:16}}
         >
-            <Form.Item label ="Email" name="email">
+            <Form.Item label ="Email" name="email"
+                rules={[{required: true, message: 'Please enter a valid email'}]}
+            >
                 <Input/>
             </Form.Item>
-            <Form.Item label ="Password" name="password">
+            <Form.Item label ="Password" name="password"
+                rules={[{required: true, message: 'Please enter your password'}]}
+            >
                 <Input.Password/>
             </Form.Item>
             <Form.Item
